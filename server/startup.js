9c83af7bc411ae1d07a2fcca1e2944a294408ee9
Sascha Koch <sascha.koch@web.de>
2016-05-08 13:11:21 +0200

    startup data modified

--- server/startup.js
+++ server/startup.js
@@ -1,6 +1,21 @@
 Meteor.startup(function () {
   // code to run on server at startup
 
+  if (!Meteor.users.findOne()) {
+  	console.log("No users yet. Creating starter data.");
+
+  	// angler1@test.com / test123
+  	Meteor.users.insert(
+  		{ "_id" : "BRHELQCQNGw7ikoak", "createdAt" : ISODate("2016-05-08T11:06:14.485Z"), "services" : { "password" : { "bcrypt" : "$2a$10$kP5NM6FURKlz0A2J1.Q0yOgP8vCaqIdHmm5UkZ08XgPw2q4CiDSxm" }, "resume" : { "loginTokens" : [ ] } }, "emails" : [ { "address" : "angler1@test.com", "verified" : false } ] }
+  	);
+
+  	// angler2@test.com / test123
+  	Meteor.users.insert(
+  		{ "_id" : "RKWu4Ki9mWrSWTqys", "createdAt" : ISODate("2016-05-08T11:09:05.300Z"), "services" : { "password" : { "bcrypt" : "$2a$10$9/6LP6ifU0i2QU1fE.l3oueTQEPcgnUcQBOrb3QDj/3xKsNf.mwmq" }, "resume" : { "loginTokens" : [ { "when" : ISODate("2016-05-08T11:09:05.304Z"), "hashedToken" : "OfKWTURY7Y9kxX/P756zWqQv00O4v9UTfE7lHzkGs5g=" } ] } }, "emails" : [ { "address" : "angler2@test.com", "verified" : false } ] }
+  	);
+
+  }
+
   if (!FishingSites.findOne()){
 	console.log("No fishing sites yet. Creating starter data.");
 
@@ -12,7 +27,7 @@ Meteor.startup(function () {
 		crowdedness: 2,
 		cleanliness: 2,
 		createdOn: new Date(),
-		people: ['1'],
+		people: ['BRHELQCQNGw7ikoak'],
 	});
 	FishingSites.insert({
 		coord : {
@@ -22,7 +37,7 @@ Meteor.startup(function () {
 		crowdedness: 1,
 		cleanliness: 3,
 		createdOn: new Date(),
-		people: ['2'],
+		people: ['RKWu4Ki9mWrSWTqys'],
 	});
   }
 
@@ -37,7 +52,7 @@ Meteor.startup(function () {
 		type : 'trout',
 		length: 25,
 		createdOn: new Date(),
-		user: '1',
+		user: 'BRHELQCQNGw7ikoak',
 	});
 	Fish.insert({
 		coord : {
@@ -47,7 +62,7 @@ Meteor.startup(function () {
 		type : 'trout',
 		length: 30,
 		createdOn: new Date(),
-		user: '2',
+		user: 'RKWu4Ki9mWrSWTqys',
 	});
   }
 
